8f16cf362caa03e653a790f0636c388ff983139b
sarshahidzai <sarshahidzai@gmail.com>
2025-11-20 23:01:57 +0000

    Update health.js

--- api/health.js
+++ api/health.js
@@ -1,3 +1,8 @@
-export default async function handler(req, res) {
-  res.status(200).json({ status: "ok" });
-}
+// api/health.js
+module.exports = (req, res) => {
+  res.status(200).json({
+    ok: true,
+    message: "CRICAI backend is alive ✅",
+    time: new Date().toISOString(),
+  });
+};
