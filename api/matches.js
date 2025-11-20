feaf42859981b36344754cff8aaf9a15d26d75f8
sarshahidzai <sarshahidzai@gmail.com>
2025-11-20 22:34:59 +0000

    Add files via upload

---
+++ api/matches.js
@@ -0,0 +1,10 @@
+export default async function handler(req, res) {
+  try {
+    const API_KEY = process.env.CRICAPI_KEY;
+    const response = await fetch(`https://api.cricapi.com/v1/matches?apikey=${API_KEY}`);
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+}
